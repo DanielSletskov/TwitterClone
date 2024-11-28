db18b0ee67e6d12babca9c7f99ab946b2b212cce
Danie <danielsletskov@gmail.com>
2024-11-28 10:15:38 +0100

    set change push

--- TweetService/Models/Tweets.cs
+++ TweetService/Models/Tweets.cs
@@ -1,11 +1,12 @@
 namespace TwitterClone.Models;
 
-public class Post
+public class Tweets
 {
     public int Id { get; set; }
     public string Content { get; set; }
     public DateTime CreatedAt { get; set; }
     public int UserId { get; set; }
     public User User { get; set; }
+
     public ICollection<Like> Likes { get; set; }
 }
--- TwitterClone/Models/Follow.cs
+++
@@ -1,9 +0,0 @@
-namespace TwitterClone.Models;
-
-public class Follow
-{
-    public int FollowerId { get; set; }
-    public User Follower { get; set; }
-    public int FollowingId { get; set; }
-    public User Following { get; set; }
-}
