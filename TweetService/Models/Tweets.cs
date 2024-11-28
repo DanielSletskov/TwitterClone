91fca0c135b7b857d87efcf2a5d6c5a143f893b5
Danie <danielsletskov@gmail.com>
2024-11-28 10:42:11 +0100

    final commit

--- TweetService/Models/Tweets.cs
+++ TweetService/Models/Tweets.cs
@@ -2,5 +2,9 @@ namespace TweetService.Models;
 
 public class Tweets
 {
+    public int Tweetid { get; set; }
+    public string? Content { get; set; }
+    public string Author { get; set; }
+    public DateTime CreatedAt { get; set; }
     
 }
--- UserFollowService/obj/Debug/net8.0/UserFollowService.AssemblyInfo.cs
+++ UserFollowService/obj/Debug/net8.0/UserFollowService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserFollowService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db18b0ee67e6d12babca9c7f99ab946b2b212cce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17910c6d1a7b93e6dceb990609ae5ee3d2a5fb08")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserFollowService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserFollowService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
